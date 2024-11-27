8a6ae76b6b324254153def82b459f4c8066f4aac
melila79 <mariliamontoya@gmail.com>
2024-11-27 23:17:03 +0100

    Readme_mas_bonus

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
 README
+En este proyecto hemos realizados los ejercicios de evalución del segundo módulo del programa de Data Analytics de Adalab
--- ejercicio_evaluacion_modulo2.sql
+++ ejercicio_evaluacion_modulo2.sql
@@ -74,7 +74,7 @@ GROUP BY
 rating;
 
 /* 10. Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas.*/ /* ok */
-/* Usamos tablas customer AS c y rental AS r
+/* Usamos tablas customer AS c y rental AS r */
 
 SELECT
 c.customer_id,
@@ -90,7 +90,7 @@ c.first_name,
 c.last_name;
 
 /* 11. Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el recuento de alquileres.*/ /* OK */
-/* -- Tablas: category AS c, film_category AS fc, inventory AS i y rate AS r */
+/* Usamos tablas: category AS c, film_category AS fc, inventory AS i y rate AS r */
 
 SELECT
 c.name AS categoria,
@@ -161,7 +161,7 @@ LEFT JOIN category AS c
 ON c.category_id = fc.category_id
 WHERE c.name = 'Family';
 
-/* 17. Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla*/ /* OK */
+/* 17. Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla film*/ /* OK */
 
 SELECT
 title AS titulo,
@@ -169,3 +169,97 @@ rating AS categoria,
 length AS durAción
 FROM film
 WHERE rating = "R" AND length > 120;
+
+/* BONUS */
+
+/* 18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas.*/ /* OK */
+/* Usamos tablas: actor AS a y film_actor AS fa */
+
+SELECT
+a.first_name,
+a.last_name,
+COUNT(fa.film_id) AS numero_peliculas
+FROM actor AS a
+LEFT JOIN film_actor AS fa
+ON a.actor_id = fa.actor_id
+GROUP BY
+a.first_name,
+a.last_name
+HAVING numero_peliculas > 10;
+
+/* 19. Hay algún actor o actriz que no apareca en ninguna película en la tabla film_actor.*/
+
+SELECT
+a.first_name,
+a.last_name
+FROM actor AS a
+LEFT JOIN film_actor AS fa
+ON a.actor_id = fa.actor_id
+WHERE fa.actor_id IS NULL; 
+
+/* 20. Encuentra las categorías de películas que tienen un promedio de duración superior a 120 minutos y muestra el nombre de la categoría junto con el promedio de duración.*/
+
+SELECT
+c.name as categoria,
+AVG(f.length) AS promedio_duracion
+FROM category AS c
+LEFT JOIN film_category AS fc
+ON c.category_id = fc.category_id
+LEFT JOIN film AS f
+ON f.film_id = c.category_id
+GROUP BY
+name
+HAVING promedio > 120;
+
+/* 21. Encuentra los actores que han actuado en al menos 5 películas y muestra el nombre del actor junto con la cantidad de películas en las que han actuado.*/
+
+SELECT
+a.first_name,
+a.last_name,
+COUNT(fa.film_id) AS num_peliculas
+FROM actor AS a
+LEFT JOIN film_actor AS fa
+ON a.actor_id = fa.actor_id
+GROUP BY
+a.first_name,
+a.last_name
+HAVING num_peliculas >= 5;
+
+/* 22. Encuentra el título de todas las películas que fueron alquiladas por más de 5 días. Utiliza una subconsulta para encontrar los rental_ids con una duración superior a 5 días */
+/* y luego selecciona las películas correspondientes.*/
+/* Usamos tablas: film AS f, rental AS r */
+
+SELECT
+f.title
+FROM film AS f
+WHERE f.film_id IN (
+	SELECT
+    rental_id
+    FROM rental AS r
+    GROUP BY
+    r.rental_id
+    HAVING COUNT(r.rental_id) > 5
+    );
+
+/* 23. Encuentra el nombre y apellido de los actores que no han actuado en ninguna película de la categoría "Horror". Utiliza una subconsulta para encontrar los actores que han actuado en películas de la categoría "Horror" y luego exclúyelos de la lista de actores.*/
+
+
+/* 24. Encuentra el título de las películas que son comedias y tienen una duración mayor a 180 minutos en la tabla film.*/ /* OK */
+
+SELECT
+f.title AS titulo,
+c.name,
+f.length AS duración
+FROM film AS f
+LEFT JOIN film_category AS fc
+ON fc.film_id = f.film_id
+LEFT JOIN category AS c
+ON fc.category_id = c.category_id
+WHERE f.length > 180 AND c.name = 'comedy';
+
+/* 25. Encuentra todos los actores que han actuado juntos en al menos una película. La consulta debe mostrar el nombre y apellido de los actores y el número de películas en las que han actuado juntos.*/
+
+SELECT
+a.first_name,
+a.last_name
+FROM actor AS A
